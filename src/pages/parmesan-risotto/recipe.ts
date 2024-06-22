6379445c922368f1cebe16393b5cd05287511018
Daniel Mellado <mellado.m2@gmail.com>
2024-06-22 10:29:59 +0200

    fix typo

--- src/pages/parmesan-risotto/recipe.ts
+++ src/pages/parmesan-risotto/recipe.ts
@@ -21,7 +21,7 @@ export const parmesanRisotto: Recipe = {
     {
       steps: [
         "Poner cazuela al fuego",
-        "Poner el arroz en la cazuela para iniciar la tostatura. (seco, solo el arroz al fuego",
+        "Poner el arroz en la cazuela para iniciar la tostatura. (seco, solo el arroz al fuego)",
         "Añadir sal y piemienta durante la tostatura",
         "La tostatura termina cuando con la mano no puedes tocar el arroz por que esta caliente",
         "Se añade agua hirviendo poco a poco, intentando mantener el arroz siempre cubierto por el líquido",
