0a6756a1c1a971a92c8de0146cf1bf26261a2dff
Daniel Mellado <mellado.m2@gmail.com>
2024-08-25 12:23:38 +0200

    feat: refactor color variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			bgPrimary: "#f1eddd",
+			bgSecondary: "#eee29d",
+			black: "#230f0f",
+			primary: "#3b1a18",
+		}
 	},
 	plugins: [],
 }
