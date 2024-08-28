69ea8b096df95d3b18d243d32fb5b8523743bdc9
Daniel Mellado <mellado.m2@gmail.com>
2024-08-28 17:43:59 +0200

    feat: format codebase

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,23 +1,23 @@
 export interface Recipe {
-  title: string;
-  description?: string;
-  cookingTime: number;
-  ingredients: IngredientGroup[];
-  steps: stepsGroup[];
-  picture: string;
-  videoId?: string;
+	title: string
+	description?: string
+	cookingTime: number
+	ingredients: IngredientGroup[]
+	steps: stepsGroup[]
+	picture: string
+	videoId?: string
 }
 
 export interface IngredientGroup {
-  title?: string;
-  ingredients: string[];
+	title?: string
+	ingredients: string[]
 }
 
 export interface stepsGroup {
-  title?: string;
-  steps: string[];
+	title?: string
+	steps: string[]
 }
 
 export interface RecipeWithId extends Recipe {
-  id: string;
+	id: string
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 		colors: {
@@ -8,8 +8,8 @@ export default {
 			bgSecondary: "#eee29d",
 			black: "#230f0f",
 			primary: "#3b1a18",
-			secondary: "#B05138"
-		}
+			secondary: "#B05138",
+		},
 	},
 	plugins: [],
 }
