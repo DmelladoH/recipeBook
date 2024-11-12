412bebc5ab9297de0e34c62bc0c3d99eb7fbda86
Daniel Mellado <mellado.m2@gmail.com>
2024-11-12 23:51:35 +0100

    feat: remove lentejas

---
+++ src/lib/backUp.ts
@@ -0,0 +1,49 @@
+const foo = {
+	id: "lentejas",
+	title: "Lentejas",
+	cookingTime: 120,
+	ingredients: [
+		{
+			title: "Sofrito",
+			ingredients: ["Tomates (triturado o naturales)", "Cebolla", "Pimentón"],
+		},
+		{
+			title: "Lentejas",
+			ingredients: [
+				"300g de lentejas",
+				"50g de sobrasada",
+				"1 cebolla",
+				"1 zanahoria",
+				"1 patata",
+				"Caldo (de carne o ave, según preferencia)",
+			],
+		},
+	],
+	steps: [
+		{
+			title: "Preparación previa",
+			steps: ["Dejamos las lentejas remojando un par de horas."],
+		},
+		{
+			title: "Cocción",
+			steps: [
+				"En una olla, calentar un poco de aceite con el sofrito. Cocinar hasta que esté caliente.",
+				"Una vez el sofrito esté caliente, incorporar la sobrasada y mezclar bien.",
+				"Apagar el fuego, ya que las lentejas deben comenzar a cocinarse desde frío.",
+				"Añadir las lentejas previamente remojadas.",
+				"Cortar la patata en trozos grandes y añadirla a la olla.",
+				"Incorporar la cebolla partida por la mitad.",
+				"Agregar la zanahoria cortada en trozos grandes.",
+				"Verter el caldo, complementando con un poco de agua si es necesario.",
+				"Sazonar ligeramente, dejando espacio para ajustar el punto de sal al final de la cocción.",
+				"Encender el fuego y llevar a ebullición.",
+				"Una vez que empiece a hervir, retirar las impurezas que puedan flotar en la superficie del caldo.",
+				"Bajar el fuego y dejar que las lentejas se cocinen a fuego lento durante 45 minutos a 1 hora.",
+				"Retirar las verduras cocidas, triturarlas junto con un poco del líquido y la grasa que se haya formado en la superficie.",
+				"Una vez triturado, añadir esta mezcla a las lentejas y mezclar hasta integrar bien.",
+				"Ajustar el punto de sal y servir.",
+			],
+		},
+	],
+	picture: "",
+}
