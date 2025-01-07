84c41d7f0e783bdc55df12b77f787c6eb00e4d0c
Daniel Mellado <mellado.m2@gmail.com>
2025-01-07 18:27:51 +0100

    feat: update astro and install mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind"
 
 import vercel from "@astrojs/vercel/serverless"
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	output: "server",
-	adapter: vercel(),
+    integrations: [tailwind(), mdx()],
+    output: "server",
+    adapter: vercel(),
 })
