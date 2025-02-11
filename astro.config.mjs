43173c211eb637254c04f8eb7b96522e69ea68e6
Daniel Mellado <mellado.m2@gmail.com>
2025-02-11 20:59:31 +0100

    feat: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import vercel from "@astrojs/vercel/serverless"
 
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx()],
+    integrations: [tailwind(), mdx(), react()],
     output: "server",
     adapter: vercel(),
 })
