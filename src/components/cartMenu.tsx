65f5357f8e371a962e1f4765b53a6b6f036d3df0
Daniel Mellado <mellado.m2@gmail.com>
2025-02-28 18:35:51 +0100

    feat: add cart empty state

--- src/components/cartMenu.tsx
+++ src/components/cartMenu.tsx
@@ -30,33 +30,7 @@ export default function CartMenu() {
 
 	return (
 		<>
-			<button
-				onClick={toggleMenu}
-				className="fixed right-8 top-8 z-50 rounded-full bg-bgPrimary p-2 hover:scale-110 hover:transform"
-				aria-controls="cart-menu"
-				aria-expanded={isOpen}
-			>
-				<span className="sr-only">Open Cart</span>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-				>
-					<path d="m15 11-1 9"></path>
-					<path d="m19 11-4-7"></path>
-					<path d="M2 11h20"></path>
-					<path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4"></path>
-					<path d="M4.5 15.5h15"></path>
-					<path d="m5 11 4-7"></path>
-					<path d="m9 11 1 9"></path>
-				</svg>
-			</button>
+			<CartButton toggleMenu={toggleMenu} isOpen={isOpen} />
 			<aside
 				className={`fixed inset-y-0 right-0 z-40 overflow-scroll bg-bgSecondary transition-transform duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"} w-full sm:w-[60%] lg:w-[30%]`}
 			>
@@ -64,30 +38,77 @@ export default function CartMenu() {
 					<header>
 						<h3 className="text-center text-xl uppercase">Cart</h3>
 					</header>
-					<section className="cart p-4">
-						{
-							<ul>
-								{ingredients.map((ingredient) => {
-									return (
-										<li key={ingredient.ingredient}>
-											{ingredient.ingredient} x {ingredient.quantity}{" "}
-											{ingredient.unit}
-										</li>
-									)
-								})}
-							</ul>
-						}
+					<section className="cart mt-10 p-4">
+						{ingredients.length > 0 ? (
+							<IngredientList ingredients={ingredients} />
+						) : (
+							<p className="text-center">El carro esta vacio</p>
+						)}
 					</section>
 					<div className="mt-10 flex justify-center">
-						<button
-							className="rounded border-2 px-4 py-2"
-							onClick={removeAllFromCart}
-						>
-							Borrar todo
-						</button>
+						{ingredients.length > 0 && (
+							<button
+								className="rounded border-2 px-4 py-2"
+								onClick={removeAllFromCart}
+							>
+								Borrar todo
+							</button>
+						)}
 					</div>
 				</div>
 			</aside>
 		</>
+	)
+}
+
+function IngredientList({ ingredients }: { ingredients: Item[] }) {
+	return (
+		<ul className="grid gap-1">
+			{ingredients.map((ingredient) => {
+				return (
+					<li key={ingredient.ingredient}>
+						{ingredient.ingredient} x {ingredient.quantity} {ingredient.unit}
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+function CartButton({
+	toggleMenu,
+	isOpen,
+}: {
+	toggleMenu: () => void
+	isOpen: boolean
+}) {
+	return (
+		<button
+			onClick={toggleMenu}
+			className="fixed right-8 top-8 z-50 rounded-full bg-bgPrimary p-2 hover:scale-110 hover:transform"
+			aria-controls="cart-menu"
+			aria-expanded={isOpen}
+		>
+			<span className="sr-only">Open Cart</span>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path d="m15 11-1 9"></path>
+				<path d="m19 11-4-7"></path>
+				<path d="M2 11h20"></path>
+				<path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4"></path>
+				<path d="M4.5 15.5h15"></path>
+				<path d="m5 11 4-7"></path>
+				<path d="m9 11 1 9"></path>
+			</svg>
+		</button>
 	)
 }
